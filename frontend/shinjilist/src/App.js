28c9faf6035668d180b9a637605ebc951fa36205
chara1236 <charliezhao06@gmail.com>
2023-11-18 09:17:27 -0500

    Tryna start the react frontend stuff

--- frontend/shinjilist/src/App.js
+++ frontend/shinjilist/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import HomeScreen from './src/Homes.js'
+import ListScreen from './src/List.js'
 
-export default App;
+const navigator = createStackNavigator ({
+  Home: HomeScreen,
+  List: ListScreen
+}, {
+  initialRouteName: 'Home',
+  defaultNavigationOptions: {
+    headerShown: false
+  }
+});
+
+export default createAppContainer(navigator)
