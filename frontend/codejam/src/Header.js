4238f89d39a29992c83528e9e889adfe6dd5ece1
chara1236 <charliezhao06@gmail.com>
2023-11-19 01:01:30 -0500

    bruh

--- frontend/codejam/src/Header.js
+++ frontend/codejam/src/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <header>
             <img className="logo" src="kinnieplaylistlogo.svg"/>
-            {/* <img className="Header-spotify" src='spotify-icons-logos/logos/01_RGB/02_PNG/Spotify_Logo_RGB_Green.png' /> */}
+            <img className="Header-spotify" src='spotify-icons-logos/logos/01_RGB/02_PNG/Spotify_Logo_RGB_Green.png' />
         </header>
     );
 }
