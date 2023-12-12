1505d665b58bb6d5380cba8e2ac7f8aef90eb3a1
kevinjycui <kevinjycui@protonmail.com>
2023-12-12 18:32:18 -0500

    Add character temp admin function

---
+++ backend/admin.py
@@ -0,0 +1,60 @@
+# Add character
+
+import sys
+
+import mysql.connector
+import yaml
+
+if __name__ != '__main__':
+    sys.exit()
+
+with open('config.yml', 'r') as file:
+    config = yaml.safe_load(file)
+
+conn = mysql.connector.connect(
+    host = config['mysql']['host'],
+    port = config['mysql']['port'],
+    user = config['mysql']['root']['user'],
+    password = config['mysql']['root']['password'])
+
+cur = conn.cursor()
+cur.execute('USE kinnie')
+
+current_media = ''
+
+while True:
+    if input('Add another character? (y/n) ').lower() != 'y':
+        break
+
+    name = input('\tName: ')
+    name = name.strip()
+
+    character_id = input('\tID (Leave blank to set name as ID): ')
+    character_id = character_id.strip()
+    if len(character_id) == 0:
+        character_id = name.replace(' ', '_')
+
+    img_file = input('\tImage URL: ')
+    img_file = img_file.strip()
+
+    media = input('\tMedia (Leave blank to set same as last): ' if len(current_media) > 0 else '\tMedia: ')
+    media = media.strip()
+    if len(media) == 0 and len(current_media) > 0:
+        media = current_media
+    current_media = media
+
+    cmd = '''INSERT IGNORE INTO characters SET
+                        character_id = %s,
+                        name = %s,
+                        img_file = %s,
+                        media = %s'''
+    print(cmd % (character_id, name, img_file, media))
+
+    cur = conn.cursor()
+    cur.execute(cmd, (character_id, name, img_file, media))
+
+    if input('Is this information correct? (y/n) ').lower() != 'y':
+        conn.rollback()
+    else:
+        conn.commit()
+        print('Added successfully!')
--- frontend/kinnie-playlist/src/elements/AddSong.js
+++ frontend/kinnie-playlist/src/elements/AddSong.js
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useParams } from 'react-router';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { faAngleUp, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 import { apiJson } from '../api/apiUtil'
 import { TokenContext, TrackContext } from '../AuthRoute'
@@ -20,6 +20,7 @@ function AddSong() {
 
     const [loading, setLoading] = useState('');
     const [scrolled, setScrolled] = useState(false);
+    const [added, setAdded] = useState('');
 
     const [playlist, setPlaylist] = useContext(PlaylistContext);
 
@@ -59,10 +60,13 @@ function AddSong() {
             }
         }
 
+        setAdded(addSong.response.song_id);
+
         newPlaylist.unshift({
             song_id: addSong.response.song_id,
             number_of_users: 1
         });
+
         setPlaylist(newPlaylist);
         setLoading('');
     }
@@ -74,7 +78,7 @@ function AddSong() {
     return (
         token != null && current_track !== track ?
             <button className={"AddSong" + (scrolled ? " AddSong-scrolled" : "")} onClick={addCurrentSong} >
-                <FontAwesomeIcon className="AddSong-icon" icon={faAngleUp} />
+                <FontAwesomeIcon className="AddSong-icon" icon={current_track.song_id === added ? faCheck : faAngleUp} />
                 <div className="Addsong-text">{
                     loading === '' ?
                         <>Vote <b>{current_track.title}</b> by {current_track.artists} for this character's theme!</> :
