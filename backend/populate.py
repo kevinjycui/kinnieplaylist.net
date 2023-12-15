a14e985d65aff8274ff4361af5dee2d450c2ff15
kevinjycui <kevinjycui@protonmail.com>
2023-12-15 12:45:55 -0500

    Hide dangerous config files

--- backend/populate.py
+++ backend/populate.py
@@ -1,135 +1,135 @@
-# Initial data population
+# # Initial data population
 
-import sys
+# import sys
 
-import mysql.connector
-import yaml
+# import mysql.connector
+# import yaml
 
-if __name__ != '__main__':
-    sys.exit()
+# if __name__ != '__main__':
+#     sys.exit()
 
-with open('config.yml', 'r') as file:
-    config = yaml.safe_load(file)
+# with open('config.yml', 'r') as file:
+#     config = yaml.safe_load(file)
 
-conn = mysql.connector.connect(
-    host = config['mysql']['host'],
-    port = config['mysql']['port'],
-    user = config['mysql']['root']['user'],
-    password = config['mysql']['root']['password'])
+# conn = mysql.connector.connect(
+#     host = config['mysql']['host'],
+#     port = config['mysql']['port'],
+#     user = config['mysql']['root']['user'],
+#     password = config['mysql']['root']['password'])
 
-def sql_run_logged(cmd):
-    print(cmd, end=';\n')
-    cur = conn.cursor()
-    cur.execute(cmd)
+# def sql_run_logged(cmd):
+#     print(cmd, end=';\n')
+#     cur = conn.cursor()
+#     cur.execute(cmd)
 
-sql_run_logged('USE kinnie')
+# sql_run_logged('USE kinnie')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Shinji_Ikari\',
-                    name = \'Shinji Ikari\',
-                    img_file = \'https://i.imgur.com/3m1mVBm.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Shinji_Ikari\',
+#                     name = \'Shinji Ikari\',
+#                     img_file = \'https://i.imgur.com/3m1mVBm.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Kaworu_Nagisa\',
-                    name = \'Kaworu Nagisa\',
-                    img_file = \'https://i.imgur.com/0sJd9Tt.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Kaworu_Nagisa\',
+#                     name = \'Kaworu Nagisa\',
+#                     img_file = \'https://i.imgur.com/0sJd9Tt.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Rei_Ayanami\',
-                    name = \'Rei Ayanami\',
-                    img_file = \'https://i.imgur.com/EVvuDSU.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Rei_Ayanami\',
+#                     name = \'Rei Ayanami\',
+#                     img_file = \'https://i.imgur.com/EVvuDSU.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Asuka_Langley_Souryuu\',
-                    name = \'Asuka Langley Souryuu\',
-                    img_file = \'https://i.imgur.com/sZUj5BJ.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Asuka_Langley_Souryuu\',
+#                     name = \'Asuka Langley Souryuu\',
+#                     img_file = \'https://i.imgur.com/sZUj5BJ.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Misato_Katsuragi\',
-                    name = \'Misato Katsuragi\',
-                    img_file = \'https://i.imgur.com/66m0GU3.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Misato_Katsuragi\',
+#                     name = \'Misato Katsuragi\',
+#                     img_file = \'https://i.imgur.com/66m0GU3.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Ritsuko_Akagi\',
-                    name = \'Ritsuko Akagi\',
-                    img_file = \'https://i.imgur.com/bv4fbnl.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Ritsuko_Akagi\',
+#                     name = \'Ritsuko Akagi\',
+#                     img_file = \'https://i.imgur.com/bv4fbnl.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Maya_Ibuki\',
-                    name = \'Maya Ibuki\',
-                    img_file = \'https://i.imgur.com/1OQM4qX.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Maya_Ibuki\',
+#                     name = \'Maya Ibuki\',
+#                     img_file = \'https://i.imgur.com/1OQM4qX.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Gendou_Ikari\',
-                    name = \'Gendou Ikari\',
-                    img_file = \'https://i.imgur.com/eN9jD8N.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Gendou_Ikari\',
+#                     name = \'Gendou Ikari\',
+#                     img_file = \'https://i.imgur.com/eN9jD8N.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Kaji_Ryouji\',
-                    name = \'Kaji Ryouji\',
-                    img_file = \'https://i.imgur.com/PpzU8FX.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Kaji_Ryouji\',
+#                     name = \'Kaji Ryouji\',
+#                     img_file = \'https://i.imgur.com/PpzU8FX.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Pen_Pen\',
-                    name = \'Pen Pen\',
-                    img_file = \'https://i.imgur.com/FHIFauH.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Pen_Pen\',
+#                     name = \'Pen Pen\',
+#                     img_file = \'https://i.imgur.com/FHIFauH.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Kensuke_Aida\',
-                    name = \'Kensuke Aida\',
-                    img_file = \'https://i.imgur.com/IRu8VMV.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Kensuke_Aida\',
+#                     name = \'Kensuke Aida\',
+#                     img_file = \'https://i.imgur.com/IRu8VMV.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Touji Suzuhara\',
-                    name = \'Touji Suzuhara\',
-                    img_file = \'https://i.imgur.com/llvfrP7.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Touji Suzuhara\',
+#                     name = \'Touji Suzuhara\',
+#                     img_file = \'https://i.imgur.com/llvfrP7.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Kouzou_Fuyutsuki\',
-                    name = \'Kouzou Fuyutsuki\',
-                    img_file = \'https://i.imgur.com/CvxDPlh.jpg\',
-                    media = \'Neon Genesis Evangelion\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Kouzou_Fuyutsuki\',
+#                     name = \'Kouzou Fuyutsuki\',
+#                     img_file = \'https://i.imgur.com/CvxDPlh.jpg\',
+#                     media = \'Neon Genesis Evangelion\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Lain_Iwakura\',
-                    name = \'Lain Iwakura\',
-                    img_file = \'https://i.imgur.com/b1FgBKH.jpg\',
-                    media = \'Serial Experiments Lain\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Lain_Iwakura\',
+#                     name = \'Lain Iwakura\',
+#                     img_file = \'https://i.imgur.com/b1FgBKH.jpg\',
+#                     media = \'Serial Experiments Lain\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Ayumu_Kasuga_(Osaka)\',
-                    name = \'Ayumu "Osaka" Kasuga\',
-                    img_file = \'https://i.imgur.com/pyD4Z0G.jpg\',
-                    media = \'Azumanga Daioh\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Ayumu_Kasuga_(Osaka)\',
+#                     name = \'Ayumu "Osaka" Kasuga\',
+#                     img_file = \'https://i.imgur.com/pyD4Z0G.jpg\',
+#                     media = \'Azumanga Daioh\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Sakaki\',
-                    name = \'Sakaki\',
-                    img_file = \'https://i.imgur.com/bNzYjI8.jpg\',
-                    media = \'Azumanga Daioh\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Sakaki\',
+#                     name = \'Sakaki\',
+#                     img_file = \'https://i.imgur.com/bNzYjI8.jpg\',
+#                     media = \'Azumanga Daioh\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Chiyo_Mihama\',
-                    name = \'Chiyo Mihama\',
-                    img_file = \'https://i.imgur.com/wX1uV5Z.jpg\',
-                    media = \'Azumanga Daioh\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Chiyo_Mihama\',
+#                     name = \'Chiyo Mihama\',
+#                     img_file = \'https://i.imgur.com/wX1uV5Z.jpg\',
+#                     media = \'Azumanga Daioh\'''')
 
-sql_run_logged('''INSERT IGNORE INTO characters SET
-                    character_id = \'Tomo_Takino\',
-                    name = \'Tomo Takino\',
-                    img_file = \'https://i.imgur.com/sTz8zfJ.jpg\',
-                    media = \'Azumanga Daioh\'''')
+# sql_run_logged('''INSERT IGNORE INTO characters SET
+#                     character_id = \'Tomo_Takino\',
+#                     name = \'Tomo Takino\',
+#                     img_file = \'https://i.imgur.com/sTz8zfJ.jpg\',
+#                     media = \'Azumanga Daioh\'''')
 
-conn.commit()
+# conn.commit()
--- backend/setup.py
+++ backend/setup.py
@@ -1,72 +1,70 @@
-import sys
+# import sys
 
-import mysql.connector
-import yaml
+# import mysql.connector
+# import yaml
 
-if __name__ != '__main__':
-    sys.exit()
+# if __name__ != '__main__':
+#     sys.exit()
 
-with open('config.yml', 'r') as file:
-    config = yaml.safe_load(file)
+# with open('config.yml', 'r') as file:
+#     config = yaml.safe_load(file)
 
-conn = mysql.connector.connect(
-    host = config['mysql']['host'],
-    port = config['mysql']['port'],
-    user = config['mysql']['root']['user'],
-    password = config['mysql']['root']['password'])
+# conn = mysql.connector.connect(
+#     host = config['mysql']['host'],
+#     port = config['mysql']['port'],
+#     user = config['mysql']['root']['user'],
+#     password = config['mysql']['root']['password'])
 
-def sql_run_logged(cmd, args=tuple()):
-    print(cmd, end=';\n')
-    cur = conn.cursor()
-    if len(args) == 0:
-        cur.execute(cmd)
-    else:
-        cur.execute(cmd, args)
+# def sql_run_logged(cmd, args=tuple()):
+#     print(cmd, end=';\n')
+#     cur = conn.cursor()
+#     if len(args) == 0:
+#         cur.execute(cmd)
+#     else:
+#         cur.execute(cmd, args)
 
-sql_run_logged('DROP DATABASE IF EXISTS kinnie')
-sql_run_logged('CREATE DATABASE kinnie')
+# sql_run_logged('CREATE DATABASE kinnie')
 
-sql_run_logged('USE kinnie')
+# sql_run_logged('USE kinnie')
 
-sql_run_logged('''CREATE TABLE characters 
-                    (character_id VARCHAR(255) NOT NULL,
-                    name VARCHAR(255),
-                    img_file VARCHAR(255),
-                    media VARCHAR(255),
+# sql_run_logged('''CREATE TABLE characters 
+#                     (character_id VARCHAR(255) NOT NULL,
+#                     name VARCHAR(255),
+#                     img_file VARCHAR(255),
+#                     media VARCHAR(255),
 
-                    PRIMARY KEY(character_id))
-                 CHARACTER SET utf8 COLLATE utf8_unicode_ci''')
+#                     PRIMARY KEY(character_id))
+#                  CHARACTER SET utf8 COLLATE utf8_unicode_ci''')
 
-sql_run_logged('''CREATE TABLE songs 
-                    (song_id VARCHAR(255),
-                    title VARCHAR(255), 
-                    img_file VARCHAR(255), 
-                    artists VARCHAR(255),
-                    genres VARCHAR(255),
-                    explicit BOOL,
-                    duration INT,
+# sql_run_logged('''CREATE TABLE songs 
+#                     (song_id VARCHAR(255),
+#                     title VARCHAR(255), 
+#                     img_file VARCHAR(255), 
+#                     artists VARCHAR(255),
+#                     genres VARCHAR(255),
+#                     explicit BOOL,
+#                     duration INT,
 
-                    PRIMARY KEY(song_id))
-                 CHARACTER SET utf8 COLLATE utf8_unicode_ci''')
+#                     PRIMARY KEY(song_id))
+#                  CHARACTER SET utf8 COLLATE utf8_unicode_ci''')
 
-sql_run_logged('''CREATE TABLE character_song_connections 
-                    (
-                    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,   
+# sql_run_logged('''CREATE TABLE character_song_connections 
+#                     (
+#                     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,   
 
-                    song_id VARCHAR(255),
-                    character_id VARCHAR(255),
-                    user_id VARCHAR(255))''')
+#                     song_id VARCHAR(255),
+#                     character_id VARCHAR(255),
+#                     user_id VARCHAR(255))''')
 
-app_user = config['mysql']['app']['user']
-app_pswd = config['mysql']['app']['password']
+# app_user = config['mysql']['app']['user']
+# app_pswd = config['mysql']['app']['password']
 
-sql_run_logged('DROP USER IF EXISTS %s', (app_user,))
-sql_run_logged('CREATE USER %s IDENTIFIED BY %s', (app_user, app_pswd))
-sql_run_logged('GRANT SELECT ON kinnie.* TO %s', (app_user,))
-sql_run_logged('GRANT INSERT ON kinnie.songs TO %s', (app_user,))
-sql_run_logged('GRANT INSERT ON kinnie.character_song_connections TO %s', (app_user,))
-sql_run_logged('GRANT DELETE ON kinnie.character_song_connections TO %s', (app_user,))
+# sql_run_logged('CREATE USER %s IDENTIFIED BY %s', (app_user, app_pswd))
+# sql_run_logged('GRANT SELECT ON kinnie.* TO %s', (app_user,))
+# sql_run_logged('GRANT INSERT ON kinnie.songs TO %s', (app_user,))
+# sql_run_logged('GRANT INSERT ON kinnie.character_song_connections TO %s', (app_user,))
+# sql_run_logged('GRANT DELETE ON kinnie.character_song_connections TO %s', (app_user,))
 
-sql_run_logged('FLUSH PRIVILEGES')
+# sql_run_logged('FLUSH PRIVILEGES')
 
-conn.commit()
+# conn.commit()
