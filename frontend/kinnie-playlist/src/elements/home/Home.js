180669a4f47a8c834aa8abc930c69b91f022e656
kevinjycui <kevinjycui@protonmail.com>
2024-01-04 22:36:26 -0500

    Change ordering

--- frontend/kinnie-playlist/src/elements/home/Home.js
+++ frontend/kinnie-playlist/src/elements/home/Home.js
@@ -33,8 +33,8 @@ function Home() {
                     <div className='Home-container'>
                         {filteredCharacters.length === 0 ? <div className="empty">Nobody here... Try changing your search?</div> :
                             filteredCharacters.sort((a, b) => {
-                                var nameA = a.name.toUpperCase();
-                                var nameB = b.name.toUpperCase();
+                                var nameA = a.name.toUpperCase().replace(/[^a-z0-9 ]/gi, '');
+                                var nameB = b.name.toUpperCase().replace(/[^a-z0-9 ]/gi, '');
                                 return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0
                             }).map(character => (
                                 <div className='Home-characterModule' key={character.character_id}>
