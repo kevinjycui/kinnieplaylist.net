18f2dfe0ad853dd80a25f1e3b72c149276bde601
kevinjycui <kevinjycui@protonmail.com>
2024-01-12 20:01:55 -0500

    Character count

--- frontend/kinnie-playlist/src/elements/home/Home.js
+++ frontend/kinnie-playlist/src/elements/home/Home.js
@@ -44,19 +44,23 @@ function Home() {
                     </div>
                     <div className='Home-container'>
                         {filteredCharacters.length === 0 ? <div className="empty">Nobody here... Try changing your search?</div> :
-                            filteredCharacters.sort((a, b) => {
-                                var nameA = a.name.toUpperCase().replace(/[^a-z0-9 ]/gi, '');
-                                var nameB = b.name.toUpperCase().replace(/[^a-z0-9 ]/gi, '');
-                                return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0
-                            }).map(character => (
-                                <div className='Home-characterModule' key={character.character_id}>
-                                    <Suspense fallback={<></>}>
-                                        <CharacterButton
-                                            data={character}
-                                        />
-                                    </Suspense>
-                                </div>
-                            ))}
+                        <>
+                        <div>Found {filteredCharacters.length} {filteredCharacters.length === 1 ? "character" : "characters"}</div>
+                        {filteredCharacters.sort((a, b) => {
+                            var nameA = a.name.toUpperCase().replace(/[^a-z0-9 ]/gi, '');
+                            var nameB = b.name.toUpperCase().replace(/[^a-z0-9 ]/gi, '');
+                            return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0
+                        }).map(character => (
+                            <div className='Home-characterModule' key={character.character_id}>
+                                <Suspense fallback={<></>}>
+                                    <CharacterButton
+                                        data={character}
+                                    />
+                                </Suspense>
+                            </div>
+                        ))}
+                        </>
+                        }
                     </div>
                 </div>
             </>
